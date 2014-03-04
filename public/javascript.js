b269efacae1d48d2b5a7d1792bca5dc66f91ee5f
Katie Hoffman <ktahoffman@gmail.com>
2014-03-04 00:10:06 -0500

    fixes jQuery

--- public/javascript.js
+++ public/javascript.js
@@ -2,18 +2,52 @@ $(document).ready(function(){
 
   $("div.downarrow").on("click", function(event) {
 
-    if(!$(this).hasClass("chosen")) {
-      $(this).addClass("chosen");
+    if (!$(this).hasClass("active") && (!$(this).parents().first().hasClass("disabled"))) {
+
+      // change more to less, add active class
+      $(this).text("less");
+      $(this).addClass("active");
+
+      // add grey background to project div
+      var rents = $(this).parents().first();
+      rents.addClass("clickedgrey");
+
+      // disable other projects
+      var otherProjects = $(".project").not(rents);
+      otherProjects.addClass("disabled");
+
+      // show descriptions
       var desc = $(this).siblings(".descriptionz");
-      // $(this).text("<i class="fa fa-angle-double-up"></i>");
       desc.show( "slow", function() {
-      }); 
-    } else {
-      $(this).removeClass("chosen");
-      var desc = $(this).siblings(".descriptionz");
-      // $(this).text("<i class="fa fa-angle-double-down"></i>");
-      desc.hide( "slow", function() {
       });
+
+      console.log("if");
+
+    } else if (!$(this).parents().first().hasClass("disabled")) {
+
+      // change less back to more, remove active class
+      $(this).text("more");
+      $(this).removeClass("active");
+
+      // remove grey background
+      var parents = $(this).parents().first();
+      parents.removeClass("clickedgrey");
+
+      // enable all projects
+      $(".project").removeClass("disabled");
+
+      // hide description
+      var description = $(this).siblings(".descriptionz");
+      description.hide( "slow", function() {
+      }); 
+
+      console.log("else if");
+
+    } else {
+      var clicked = $(this);
+      console.log("else: disabled");
     }
+
   });
+
 });
