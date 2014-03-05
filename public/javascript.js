c98ca8f4408d7f5e5bad971e2e831fbf991bdb3e
Katie Hoffman <ktahoffman@gmail.com>
2014-03-04 21:54:44 -0500

    allows for clients to click on other .downarrow divs even when on is active

--- public/javascript.js
+++ public/javascript.js
@@ -1,53 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $("div.downarrow").on("click", function(event) {
+  $("div.downarrow").on("click", function (event) {
 
-    if (!$(this).hasClass("active") && (!$(this).parents().first().hasClass("disabled"))) {
+    if (!$(this).hasClass("active")) {
 
-      // change more to less, add active class
+      // change less back to more for all .downarrow divs & change more to less for this downarrow
+      $("div.downarrow").text("more");
       $(this).text("less");
-      $(this).addClass("active");
 
-      // add grey background to project div
-      var rents = $(this).parents().first();
-      rents.addClass("clickedgrey");
+      // remove grey background on all .project divs & add grey background to project div
+      $("div.project").removeClass("clickedgrey");
+      var parents = $(this).parents().first();
+      parents.addClass("clickedgrey");
 
-      // disable other projects
-      var otherProjects = $(".project").not(rents);
-      otherProjects.addClass("disabled");
-
-      // show descriptions
+      // hide all descriptions & show description for this project div
+      $("div.descriptionz").hide("slow", function () {});
       var desc = $(this).siblings(".descriptionz");
-      desc.show( "slow", function() {
-      });
+      desc.show("slow", function () {});
+
+ 
+      // remove class active from all downarrow divs & add it to this downarrow div
+      $("div.downarrow").removeClass("active");
+      $(this).addClass("active");
 
       console.log("if");
 
-    } else if (!$(this).parents().first().hasClass("disabled")) {
+    } else if ($(this).hasClass("active")) {
 
-      // change less back to more, remove active class
-      $(this).text("more");
-      $(this).removeClass("active");
+      // change less back to more for all .downarrow divs
+      $("div.downarrow").text("more");
 
-      // remove grey background
-      var parents = $(this).parents().first();
-      parents.removeClass("clickedgrey");
+      // remove grey background on all .project divs
+      $("div.project").removeClass("clickedgrey");
 
-      // enable all projects
-      $(".project").removeClass("disabled");
+      // hide all descriptions
+      $("div.descriptionz").hide("slow", function () {});
 
-      // hide description
-      var description = $(this).siblings(".descriptionz");
-      description.hide( "slow", function() {
-      }); 
+      // remove class active
+      $("div.downarrow").removeClass("active");
 
-      console.log("else if");
+      console.log("else if wha???");
 
     } else {
-      var clicked = $(this);
-      console.log("else: disabled");
-    }
-
-  });
-
-});
+      console.log("else");
+      console.log(this);
+    }// else
+  }); // $("div.downarrow")
+}); // $(document).ready
